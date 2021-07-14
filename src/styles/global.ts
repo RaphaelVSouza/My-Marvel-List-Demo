c6156a8147b0a3ee361cf1fa3987f174fecd34f2
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-13 22:42:26 -0300

    Styles - add global styles file

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,18 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  html {
-    font-size: 62.5%;
-  }
-  body {
-    font-size: 16px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    * {
+      box-sizing: border-box;
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+    body {
+      font-size: 16px;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      background-color: ${theme.colors.mainBg};
+      color: #FFFFFF;
+    }
+
 `;
 
 export default GlobalStyles;
