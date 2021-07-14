23f853b6876da9fd96ec7e2c48aea19390111ead
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-13 22:47:45 -0300

    add theme, reset and global styles to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,19 @@
+import { ThemeProvider } from 'styled-components'
 import { addDecorator } from "@storybook/react";
 import GlobalStyles from "../src/styles/global";
+import ResetStyles from "../src/styles/reset";
 import StoryRouter from "storybook-react-router";
+import theme from '../src/styles/theme'
 
 addDecorator(StoryRouter());
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
+      <ResetStyles />
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   ),
 ];
 
