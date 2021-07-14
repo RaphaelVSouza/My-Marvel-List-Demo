26036da9f14c4408750708a4171208c790b5c7a2
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-13 22:35:32 -0300

    add tests to button component

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Button from '.'
+
+describe.only('<Button />', () => {
+
+  it('should render the button with small size', () => {
+    renderWithTheme(<Button size="small" />)
+
+    expect(screen.getByRole('button')).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.6rem',
+
+    })
+  })
+
+  it('should render the button with medium size', () => {
+    renderWithTheme(<Button size="medium" />)
+
+    expect(screen.getByRole('button')).toHaveStyle({
+      height: '4rem',
+      'font-size': '1.8rem',
+
+    })
+
+  })
+
+  it('should render the button with large size', () => {
+    renderWithTheme(<Button size="large" />)
+
+    expect(screen.getByRole('button')).toHaveStyle({
+      height: '5rem',
+      'font-size': '2.2rem',
+
+    })
+  })
+})
