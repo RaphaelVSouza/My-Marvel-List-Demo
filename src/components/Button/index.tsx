891f5c6b8f8967fefd04b49cac86b9cee99e2a2b
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-13 22:35:04 -0300

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidthOnMobile?: boolean
+} & ButtonTypes
+
+const Button = ({ children, size = 'medium', fullWidthOnMobile = false }: ButtonProps) => (
+  <S.Wrapper size={size} fullWidthOnMobile={fullWidthOnMobile}>
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,48 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import Media from 'styled-media-query'
+import { ButtonProps } from '.'
+
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidthOnMobile'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+  fullWidthOnMobile: () => css`
+
+    ${Media.lessThan("medium")`
+      width: 100%;
+    `}
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size, fullWidthOnMobile }) => css`
+    background-color: ${theme.colors.buttonBg};
+    color: ${theme.colors.white};
+    border: none;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    cursor: pointer;
+    transition: opacity 200ms ease-in-out;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!fullWidthOnMobile && wrapperModifiers.fullWidthOnMobile()}
+  `}
+`
