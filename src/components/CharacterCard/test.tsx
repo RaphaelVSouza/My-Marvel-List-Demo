aa19f11602b55b5a18daa9d3706c415d145fd434
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-13 23:11:17 -0300

    add character card test

---
+++ src/components/CharacterCard/test.tsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CharacterCard from '.'
+
+const TEST_IMAGE = {
+  path: "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55",
+  extension: "jpg"
+}
+
+describe('<CharacterCard />', () => {
+  it('should render the character card', () => {
+    const { container } = renderWithTheme(<CharacterCard id="1" name="Iron Man" thumbnail={TEST_IMAGE} data-testid="character-card" />)
+
+    expect(screen.getByRole('article')).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
