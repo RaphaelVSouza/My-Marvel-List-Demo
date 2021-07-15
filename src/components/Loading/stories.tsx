1184f1011c6643f1f1430f7fa27384482af5f230
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-15 11:22:00 -0300

    add stories to loading component

---
+++ src/components/Loading/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from "@storybook/react";
+import Loading from '.'
+
+export default {
+  title: 'Loading',
+  component: Loading
+} as Meta
+
+export const Default: Story = () => <Loading />
