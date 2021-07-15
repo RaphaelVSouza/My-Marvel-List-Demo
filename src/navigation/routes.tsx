6b37c6d5d432d69d0d06a47cb556b0622fc37c8c
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-15 11:50:48 -0300

    add routing with react-router-dom

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,14 @@
+import { ListCharacters } from "pages/ListCharacters";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/characters">
+          <ListCharacters />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
