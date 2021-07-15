2ccede8dfd17b8864e93f331ab62eefcdd71c6af
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-15 11:47:49 -0300

    add stories to text field component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,11 @@
+import { Story, Meta } from "@storybook/react";
+import withFormik from 'storybook-formik';
+import TextField from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  decorators: [withFormik]
+} as Meta
+
+export const Default: Story = () => <TextField label="Add some text" name="text" />
