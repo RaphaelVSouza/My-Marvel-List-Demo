de2424b2d82c5c3db517c44bc1945f6a4eec956b
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-15 11:44:47 -0300

    add container style component tests

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,12 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render the container', () => {
+    const { container } = renderWithTheme(<Container />)
+
+    expect(container).toBeInTheDocument()
+
+  })
+})
