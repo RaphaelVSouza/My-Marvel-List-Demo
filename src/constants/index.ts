12b282496acd3d83eebf5dbfec66011350f225bb
Raphael Vieira de Souza <raphael5254@outlook.com>
2021-07-18 17:01:18 -0300

    add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const PAGINATION_LIMIT = 9
+
+export const PAGINATION_MAX_ITEMS = 5
+
+export const PAGINATION_MAX_LEFT = (PAGINATION_MAX_ITEMS - 1) / 2
+
+export const MARVEL_PRIVATE_KEY = process.env
+  .REACT_APP_MARVEL_PRIVATE_KEY as string
+
+export const MARVEL_PUBLIC_KEY = process.env
+  .REACT_APP_MARVEL_PUBLIC_KEY as string
